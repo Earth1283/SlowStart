f2849c95865572dafdfdbee019e37b8c7a0fadb6
Earth1283 <joseph_cio@hotmail.com>
2026-08-04 16:51:53 +0800

    add leniency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kernel/exceptions/UnsafeToContinue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kernel/exceptions/UnsafeToContinue.java
@@ -5,6 +5,6 @@ public class UnsafeToContinue extends RuntimeException {
         super("I'm sorry Dave, I'm afraid I can't do that. This mission is too important for me to allow you to jeopardize it.");
     }
     public UnsafeToContinue(String message) {
-        super(message);
+        super(message + ", says Simon Travaglia. Feel free to ask him if you need help.");
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kernel/heuristics/BoundsCheck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kernel/heuristics/BoundsCheck.java
@@ -5,17 +5,23 @@ import org.firstinspires.ftc.teamcode.kernel.exceptions.UnsafeToContinue;
 public class BoundsCheck {
 
     public double acceptableError = 2.5;
+    public double leniency = 0.0;
 
     /*
     Is the robot outside the acceptable left range of
     (-acceptable, -acceptable)?
      */
-    public void isInBounds(int x, int y) {
-        if (x < acceptableError || y < acceptableError){
-            throw new UnsafeToContinue("The odometry has drifted too far, please inspect.");
+    public void isInBounds(double x, double y, boolean lenient) {
+        if (lenient) {
+            leniency = 6.9;
+        } else {
+            leniency = 0.0;
         }
-        if (x > 141.1 + acceptableError || y > 141.1 + acceptableError) {
-            throw new UnsafeToContinue("The odometry has drifted too far, please inspect.");
+        if (x < - acceptableError - leniency || y < -acceptableError - leniency){
+            throw new UnsafeToContinue("The odometry has drifted too far, please inspect");
+        }
+        if (x > 144 + acceptableError + leniency || y > 144 + acceptableError + leniency) {
+            throw new UnsafeToContinue("The odometry has drifted too far, please inspect");
         }
     }
 }
